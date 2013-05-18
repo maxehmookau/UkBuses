9f89efc9817ad85e7d77f7a8977d8f5c10c822de
Max Woolf <maximus@zingysaturn.co.uk>
2013-05-18 15:49:13 +0100

    adds simple rspec skeleton

--- lib/uk_buses/bus.rb
+++ lib/uk_buses/bus.rb
@@ -3,5 +3,11 @@ module UkBuses
 
     attr_accessor :route_number, :destination, :arrives
 
+    def initialize(route_number = nil, destination = nil, arrives = nil)
+      @route_number = route_number
+      @destination = destination
+      @arrives = arrives
+    end
+
   end
 end
--- lib/uk_buses/query.rb
+++ lib/uk_buses/query.rb
@@ -1,8 +1,35 @@
+require 'nokogiri'
+require 'open-uri'
+
 module UkBuses
   class Query
 
     def initialize(stop_code)
-      puts "Initialized with code: #{ stop_code }"
+      fetch_buses(stop_code)
+    end
+
+    def fetch_buses(stop_code)
+      buses = []
+      doc = Nokogiri::HTML(open("http://nextbuses.mobi/WebView/BusStopSearch/BusStopSearchResults/#{ stop_code }"))
+
+      doc.xpath('//*[@id="wrapper"]/div[4]/table[1]/tr').each do |row|
+        current_bus = {}
+          row.children.each do |child|
+
+            child_inner = child.inner_text.strip
+            current_bus[:route_number] = child_inner if child_inner.length >= 1 && child_inner.length < 5
+            current_bus[:destination] = child_inner.split(/\b(at|in|DUE)\b/).first[0..-2] if child_inner.length > 5
+            if child_inner.split(/\b(at|in|DUE)\b/)[1] == 'DUE'
+              current_bus[:arrives] = 'DUE'
+            else
+              current_bus[:arrives] = child_inner.split(/\b(at|in|DUE)\b/)[2].strip if child_inner.length > 5
+            end
+          end
+        buses << UkBuses::Bus.new(current_bus[:route_number],
+                                  current_bus[:destination],
+                                  current_bus[:arrives])
+      end
+      buses
     end
 
   end
